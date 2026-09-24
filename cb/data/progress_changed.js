ddb2faa6e3faeba445e7db59b567ebf4441adcc6
xianmeng872 <894301828@qq.com>
2026-09-25 05:00:09 +0800

    data: 审核进度变化广播自动更新(境内FC抓取)

--- cb/data/progress_changed.js
+++ cb/data/progress_changed.js
@@ -1,1 +1,1 @@
-window.PROGRESS_CHANGED = [{"stockCode": "603061", "stockName": "金海通", "changeDate": "2026-09-23"},{"stockCode": "301631", "stockName": "壹连科技", "changeDate": "2026-09-17"}];
+window.PROGRESS_CHANGED = [{"stockCode": "603061", "stockName": "金海通", "changeDate": "2026-09-23"}];
